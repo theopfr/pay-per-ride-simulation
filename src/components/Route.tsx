7049cc421f31576ac1e826cef30875ee2e5785d1
theopfr <teddypeifer@gmail.com>
2023-06-16 23:52:37 +0200

    finished train route animation, needs rework though

--- src/components/Route.tsx
+++ src/components/Route.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState, useRef } from "react"
+import ReactInterval from 'react-interval';
 
 
 
@@ -44,22 +45,68 @@ const TrainAnimation = (props: any) => {
 };
 
 
+
 export default function Route() {
 
     let [activeStationIdx, setActiveStationIdx] = useState<number>(0);
     let [trainLeftPosition, setTrainLeftPosition] = useState<number>(0);
-    let [animationStep, setAnimationStep] = useState<number>(0);
-    let [animationFinished, setAnimationFinished] = useState<boolean>(false);
+    let [trainWaiting, setTrainWaiting] = useState<boolean>(false);
+    let [trainMovementDelta, setTrainMovementDelta] = useState<number>(180);
+    let [trainStationDelta, setTrainStationDelta] = useState<number>(1);
 
-    const ROUTE_DISTANCE = 900;
     let stations: String[] = ["Station A", "Station B", "Station C", "Station D", "Station E", "Station F"]
 
+
+
+    /*let interval = setInterval(() => {
+        setTrainLeftPosition(trainLeftPosition + 180);
+        setActiveStationIdx(activeStationIdx + 1);
+        setTrainWaiting(false)
+    }, 4000);*/
+
+    
     useEffect(() => {
-        const interval = setInterval(() => {
-            setTrainLeftPosition(trainLeftPosition + 180);
-            setActiveStationIdx(activeStationIdx + 1);
-        }, 5000);
-    }, [trainLeftPosition]);
+        setTrainWaiting(true);
+    }, []);
+
+    useEffect(() => {
+        let interval: NodeJS.Timer;
+        interval = setInterval(() => {
+        
+        // I HAVE NO IDEA WHAT I DID HERE IM SO SORRYY
+
+        // handle train animation movement
+        if (activeStationIdx == 4) {
+            setTrainMovementDelta(-180);
+        }
+        else if (activeStationIdx == 3) {
+            setTrainMovementDelta(180);
+        }
+
+        // handle current station
+        if (activeStationIdx == 5) {
+            setTrainStationDelta(-1);
+        }
+        else if (activeStationIdx == 2) {
+            setTrainStationDelta(1);
+        }
+
+        setActiveStationIdx(activeStationIdx => activeStationIdx + trainStationDelta)
+        console.log(activeStationIdx, trainMovementDelta);
+
+        setTrainWaiting(true);
+        setTimeout(() => {
+            setTrainWaiting(false);
+            setTrainLeftPosition(prevPosition => prevPosition + trainMovementDelta);
+        }, 2500)
+            
+        }, 4000);
+        return () => clearInterval(interval);
+    }, [trainWaiting])
+
+
+
+
 
 
     return (
@@ -69,8 +116,8 @@ export default function Route() {
 
                 <div className="route-stations">
                     {
-                        stations.map((stationName: String) => {
-                            return <Station stationIsActive={false} stationName={stationName}/>
+                        stations.map((stationName: String, idx: number) => {
+                            return <Station stationIsActive={idx == activeStationIdx - 1 && trainWaiting} stationName={stationName}/>
                         })
                     }
                 </div>
@@ -78,8 +125,15 @@ export default function Route() {
             </div>
 
             <div className="route-actions">
-                <button className="route-action" id="get-in">einsteigen</button>
-                <button className="route-action" id="get-out" style={{float: "right"}}>aussteigen</button>
+                <button className="route-action" id="get-in" onClick={() => {
+                    if (trainWaiting) {
+                        console.log("waiting")
+                    }
+                    else {
+                        console.log("driving")
+                    }
+                }}>{trainWaiting ? "wait" : "get in"}</button>
+                <button className="route-action" id="get-in" style={{float: "right"}}>aussteigen</button>
             </div>
         </div>
     )
